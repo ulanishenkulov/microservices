fb8f9ffb72c868e48e0c7c1120188439e40f9add
Ulan <ishenkulovulan@gmail.com>
2026-01-28 09:54:38 +0600

    Convert user-service from submodule to regular folder

---
+++ api-gateway/src/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+
+@Injectable()
+export class UserClient {
+  private readonly userUrl: string;
+  constructor( private readonly configService: ConfigService) {
+    this.userUrl = this.configService.getOrThrow<string>('USER_SERVICE_URL');
+  }
+
+  async findAll() {
+    const res = await axios.get(`${this.userUrl}`);
+    return res.data;
+  }
+
+  async findOne(id: string)  {
+    const res = await axios.get(`${this.userUrl}/${id}`);
+    return res.data;
+  }
+
+  async remove(id: string) {
+    const res = await axios.delete(`${this.userUrl}/${id}`);
+    return res.data;
+  }
+}
